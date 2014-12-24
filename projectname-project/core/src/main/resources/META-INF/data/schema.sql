8021e637fe2ecaafc51ad32c5d6b06062079a562
smarusik <smarusik@gmail.com>
2014-12-24 08:13:03 +0200

    New mode Role added User model and DAO changed Role DAO added UserDAO tested.

--- projectname-project/core/src/main/resources/META-INF/data/schema.sql
+++ projectname-project/core/src/main/resources/META-INF/data/schema.sql
@@ -1,5 +1,13 @@
-CREATE TABLE IF NOT EXISTS Users (
-  id INTEGER PRIMARY KEY IDENTITY,
+CREATE TABLE IF NOT EXISTS ml_roles(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT/*IDENTITY*/,
+  name VARCHAR(50) UNIQUE NOT NULL,
+  description VARCHAR(500),
+  permissions INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS ml_users(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT/*IDENTITY*/,
+  ref_role INTEGER NOT NULL REFERENCES ml_roles(id) ON DELETE RESTRICT,
   login VARCHAR(25) UNIQUE NOT NULL,
   password VARCHAR(25) NOT NULL,
   firstName VARCHAR(50),
@@ -8,4 +16,25 @@ CREATE TABLE IF NOT EXISTS Users (
   comment VARCHAR(500),
   tmLastLogin TIMESTAMP,
   tmRegistered TIMESTAMP
-)
+);
+
+CREATE OR REPLACE VIEW ml_users_roles AS
+  SELECT
+    Users.id AS id,
+    Users.login AS login,
+    Users.password AS password,
+    Users.firstName AS firstName,
+    Users.secondName AS secondName,
+    Users.email AS email,
+    Users.comment AS comment,
+    Users.tmLastLogin AS tmLastLogin,
+    Users.tmRegistered AS tmRegistered,
+    Roles.id AS role_id,
+    Roles.name AS role_name,
+    Roles.description AS role_description,
+    Roles.permissions AS role_permissions
+  FROM
+    ml_users AS Users
+    LEFT JOIN
+    ml_roles AS Roles
+      ON Users.ref_role=Roles.id;

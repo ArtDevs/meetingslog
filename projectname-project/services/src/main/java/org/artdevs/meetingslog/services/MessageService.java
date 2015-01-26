72c4191cc4ae30f18f16636713cd81cb778e37a7
Artem L.V <artlaber@gmail.com>
2015-01-26 21:13:56 +0200

    Added MessageService interface

---
+++ projectname-project/services/src/main/java/org/artdevs/meetingslog/services/MessageService.java
@@ -0,0 +1,35 @@
+package org.artdevs.meetingslog.services;
+
+import java.util.List;
+import org.artdevs.meetingslog.core.model.Message;
+import org.artdevs.meetingslog.core.model.User;
+
+/**
+ * Created by Artem L.V. on 1/26/15.
+ */
+public interface MessageService {
+
+	public void createMessage(Message msg);
+
+	public void removeMessage(Message msg);
+
+	public void removeMessage(String messageId);
+
+	public void removeMessages(String owner);
+
+	public void removeMessages(User owner);
+
+	public void editMessage(Message msg);
+
+	public void editMessage(String messageId, String newMessageBody);
+
+	public List<Message> getAllMessages();
+
+	public List<Message> getMessagesByOwner(String ownerId);
+
+	public List<Message> getMessagesByGroup(String groupId);
+
+	public boolean isMessageEditable(String msgId);
+
+	public boolean isMessageEditable(Message msg);
+}

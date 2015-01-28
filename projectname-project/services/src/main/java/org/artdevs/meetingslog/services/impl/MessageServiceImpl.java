8d450c1912f1c115e40ea940b3a554a903f5d8fd
Artem L.V <artlaber@gmail.com>
2015-01-28 22:05:59 +0200

    Fixed remove by Id

--- projectname-project/services/src/main/java/org/artdevs/meetingslog/services/impl/MessageServiceImpl.java
+++ projectname-project/services/src/main/java/org/artdevs/meetingslog/services/impl/MessageServiceImpl.java
@@ -16,71 +16,69 @@ import java.util.List;
  */
 @Component
 public class MessageServiceImpl implements MessageService {
-    @Autowired
-    MessageDAO messageDAO;
+	@Autowired
+	MessageDAO messageDAO;
 
-    @Autowired
-    UserDAO userDAO;
+	@Autowired
+	UserDAO userDAO;
 
-    public void createMessage(Message msg) {
+	public void createMessage(Message msg) {
 
-        messageDAO.insert(msg);
-    }
+		messageDAO.insert(msg);
+	}
 
-    public void removeMessage(Message msg) {
-        messageDAO.removeById(msg.getId());
-    }
+	public void removeMessage(Message msg) {
+		messageDAO.removeById(msg.getId());
+	}
 
-    public void removeMessage(String messageId) {
-        if(messageDAO.findById(Integer.valueOf(messageId)).equals(messageId))
-        messageDAO.removeById(Integer.valueOf(messageId));
-    }
+	public void removeMessage(String messageId) {
+		if (messageDAO.findById(Integer.valueOf(messageId)) != null)
+			messageDAO.removeById(Integer.valueOf(messageId));
+	}
 
-    public void removeMessages(String owner) {
-        if(userDAO.findByLogin(owner) !=null)
-        messageDAO.removeByUser(userDAO.findByLogin(owner));
-    }
+	public void removeMessages(String owner) {
+		if (userDAO.findByLogin(owner) != null)
+			messageDAO.removeByUser(userDAO.findByLogin(owner));
+	}
 
-    public void removeMessages(User owner) {
-        if(userDAO.getAll().contains(owner))
-            messageDAO.removeByUser(owner);
-    }
+	public void removeMessages(User owner) {
+		if (userDAO.getAll().contains(owner))
+			messageDAO.removeByUser(owner);
+	}
 
-    public void editMessage(Message msg) {
-        if(msg.isReadonly()){
-            System.out.println("Can't edit a message!");
-        }
-    messageDAO.updateById(msg);
-    }
+	public void editMessage(Message msg) {
+		if (msg.isReadonly()) {
+			System.out.println("Can't edit a message!");
+		}
+		messageDAO.updateById(msg);
+	}
 
-    public void editMessage(String messageId, String newMessageBody) {
-        if(messageDAO.findById(Integer.valueOf(messageId)) != null && !(messageDAO.findById(Integer.valueOf(messageId)).isReadonly())) {
-            Message msg = messageDAO.findById(Integer.valueOf(messageId));
-            msg.setMsg_text(newMessageBody);
-        }
-        else{
-            System.out.println("Can't edit message");
-        }
+	public void editMessage(String messageId, String newMessageBody) {
+		if (messageDAO.findById(Integer.valueOf(messageId)) != null && !(messageDAO.findById(Integer.valueOf(messageId)).isReadonly())) {
+			Message msg = messageDAO.findById(Integer.valueOf(messageId));
+			msg.setMsg_text(newMessageBody);
+		} else {
+			System.out.println("Can't edit message");
+		}
 
-    }
+	}
 
-    public List<Message> getAllMessages() {
-        return messageDAO.getAll();
-    }
+	public List<Message> getAllMessages() {
+		return messageDAO.getAll();
+	}
 
-    public List<Message> getMessagesByOwner(String ownerId) {
-        List<Message> msgList = new ArrayList<Message>();
-        if (userDAO.findById(Integer.valueOf(ownerId)) != null) {
-            msgList  = messageDAO.getByUser(userDAO.findById(Integer.valueOf(ownerId)));
-            return msgList;
-        }
-        else{
-            return msgList = null;
-        }
-    }
+	public List<Message> getMessagesByOwner(String ownerId) {
+		List<Message> msgList = new ArrayList<Message>();
+		if (userDAO.findById(Integer.valueOf(ownerId)) != null) {
+			msgList = messageDAO.getByUser(userDAO.findById(Integer.valueOf(ownerId)));
+			return msgList;
+		} else {
+			return msgList = null;
+		}
+	}
 
-    public List<Message> getMessagesByGroup(String groupId) {
-        throw new UnsupportedOperationException("getMessagesByGroup Not yet implemented!!");
+	public List<Message> getMessagesByGroup(String groupId) {
+		throw new UnsupportedOperationException("getMessagesByGroup Not yet implemented!!");
 //        List<Message> msgList;
 //        if (userDAO.findById(Integer.valueOf(groupId)) != null) {
 //            msgList = messageDAO.getByUser(userDAO.findById(Integer.valueOf(groupId)));        //we don't have method getByGroup
@@ -88,22 +86,20 @@ public class MessageServiceImpl implements MessageService {
 //        } else {
 //            return msgList = null;
 //        }
-    }
+	}
 
-    public boolean isMessageEditable(String msgId) {
-        if(messageDAO.findById(Integer.valueOf(msgId)) != null && !(messageDAO.findById(Integer.valueOf(msgId)).isReadonly())){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+	public boolean isMessageEditable(String msgId) {
+		if (messageDAO.findById(Integer.valueOf(msgId)) != null && !(messageDAO.findById(Integer.valueOf(msgId)).isReadonly())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public boolean isMessageEditable(Message msg) {
-        if(msg.isReadonly()){
-            return false;
-        }
-        else
-            return true;
-    }
+	public boolean isMessageEditable(Message msg) {
+		if (msg.isReadonly()) {
+			return false;
+		} else
+			return true;
+	}
 }
